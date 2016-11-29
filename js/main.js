d002c63e67fd278d621b8f6b8d87ef423284499e
sushaolian <1243856760@qq.com>
2016-11-30 02:34:49 +0800

    Site updated: 2016-11-30 02:34:14

--- js/main.js
+++ js/main.js
@@ -152,9 +152,3 @@ var Main = (function(){
 })();
 
 $(Main.init());
-
-    if($(".instagram").length) {
-        require(['/js/photo.js', '/fancybox/jquery.fancybox.js', '/js/jquery.lazyload.js'], function(obj) {
-            obj.init();
-        });
-    }
